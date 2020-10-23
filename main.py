91328133423e44eb801ffa3ce6d3e76c0a2ae2a0
GuilhomenAguiar <guilherme.aguiar@ua.pt>
2020-10-23 22:04:35 +0100

    Some data (tuples added)

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import argparse
 from colorama import Fore
 import string
 import random
+from collections import namedtuple
+import pprint
+Input = namedtuple("Input", ['requested', 'received', 'duration'])
 
 parser = argparse.ArgumentParser(description=' This program pretend measure your typing accuracy!')
 parser.add_argument('--mv', '--max_value',
@@ -18,20 +21,28 @@ parser.add_argument('--utm',
 tempo_do = parser.parse_args()
 
 # print(tempo_do.__dict__["one"])
+TupList = []
+dictionary = {"accuracy": "", "number of hits": "", "number of types": "", "test duration": "",
+              "test start": "", "test end": "", "type average time": "", "type hit average time": "",
+              "type miss average time": "", "Every type data": ""}
 
 
 def gameOn(letters, number_typed_letters):
     letter = random.choice(letters)
-
     print("Type letter " + Fore.LIGHTBLUE_EX + str(letter) + Fore.RESET)
+    init_time = time.time()
     char = getch.getch()
+    type_time = time.time()
+    elapsed_time = type_time - init_time
     number_typed_letters += 1
     if char == letter:
         print("You typed letter " + Fore.GREEN + str(char) + Fore.RESET)
     else:
         print("You typed letter " + Fore.RED + str(char) + Fore.RESET)
-    return char
 
+    triple = Input(letter, str(char), str(elapsed_time))
+    TupList.append(triple)
+    return char
 
 
 def printAllCharsUpTo(stop_char):
@@ -57,16 +68,11 @@ def printAllCharsUpTo(stop_char):
 
     print('Here is the list of all your inputs: ' + str(inputs))
 
-    # Ex 5a Processing of list of inputs
-    total_numerics = 0
-    for input in inputs:
-        if input.isdigit():
-            total_numerics += 1
-    print('Total numeric inputs is ' + str(total_numerics))
-
 
 def main():
     number_typed_letters = 0
+    hitkeys = 0
+    misses = 0
     print("Welcome to the " + Fore.RED + "PARI " + Fore.LIGHTBLUE_EX + "Ultimate Speed Typing Test." + Fore.RESET)
     print("Please, when you're ready, press any key.")
     char = getch.getch()
@@ -86,12 +92,13 @@ def main():
     # ARGUMENTOS
 
 
-    max_value = 10
+    max_value = 5
     utm = True
 
     if not utm:
         while number_typed_letters < max_value:
             char = gameOn(letters, number_typed_letters)
+
     else:
         while time.time()-init_time < max_value:
             char = gameOn(letters, number_typed_letters)
@@ -99,10 +106,13 @@ def main():
         if time.time() - init_time <= max_value:
             print("Current test duration is " + str(time.time() - init_time))
         else:
-            print("\n" + Fore.RED + "WARNING: " + Fore.RESET + "Current test duration (" + str(time.time() - init_time) + ") exceeded the maximum of " + str(max_value) + ":")
+            print("\n" + Fore.RED + "WARNING: " + Fore.RESET + "Current test duration (" +
+                  str(time.time() - init_time) + ") exceeded the maximum of " + str(max_value) + ":")
             print("The last typed letter (" + str(char) + ") will not count")
-
     print(Fore.LIGHTBLUE_EX + "Test finished!" + Fore.RESET)
+    pprint.pprint(dictionary)
+    print('\n'.join(map(str, TupList)))
+
 
     # # not displayed in the screen
     # print('You have entered  ' + char)
