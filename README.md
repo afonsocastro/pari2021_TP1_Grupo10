a8e62d422ff3611a89723a4295e6dbb4fd47b6bd
afonsocastro <afonsocastro@ua.pt>
2020-10-25 13:04:44 +0000

    last changes: Now the spacebar interrupts the program if the utm is set true. Now the game count down as a better appearance Now the utm input argument is of type boolean Now the time type results have the word 'seconds' at his end

--- README.md
+++ README.md
@@ -1,30 +1,70 @@
-# pari2021_TP1_Grupo10
+   
+# Ultimate Speed Typing Test
+This program aims to measure your typing accuracy.
 
-Nota 1: É necessário ter um repositório git só para este trabalho. Todos os elementos do grupo devem trabalhar sobre o mesmo repo (serem colaboradores).
+# Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Results](#results)
 
-Nota 2: No dia da apresentação (quarta-feira, durante a aula) cada grupo apresentará por ordem aleatória. As apresentações serão à base de demonstração e não do código em si. No entanto, o prof pode pedir para ver o código e o repositório github
+<small><i><a href='http://ecotrust-canada.github.io/markdown-toc/'>Table of contents generated with markdown-toc</a></i></small>
 
-Nota 3: o professor pode (e vai) fazer perguntas a todos os membros do grupo, ou possívelmente aos 2 que não falarem na apresnetação. Para avaliar se todos percebem o que foi feito
+# Installation
+In order to properly use the Ultimate Speed Typing Test, you will need to install some python packages. We recommend that you install these packages using the pip command.
+If you do not have pip installed in your system, please, in a terminal window, insert the following command line:
+    
+    sudo apt install python3-pip
+    
+After you get the pip command installed, it is now time to install the required python packages for running the Ultimate Speed Typing Test.
+Please, install getch package:
 
-Nota 4: todos os elementos do mesmo grupo terão a mesma nota
+    pip install https://pypi.python.org/packages/source/g/getch/getch-1.0-python2.tar.gz#md5=586ea0f1f16aa094ff6a30736ba03c50
+   
+And, again in the terminal window, install the pyfiglet:
+    
+    pip install pyfiglet
 
-Nota 5: a aprentação será de cerca de 10 minutos, entre perguntas e explicações
 
-Trabalho (ReadME): https://github.com/miguelriemoliveira/pari_20-21/blob/master/Parte04/README.adoc
+Now, you are ready to use the Ultimate Speed Typing Test!
 
+# Usage
+To launch the program, the user can enter 2 additional input arguments: the Maximum Value and/or the User Time Mode.
+If none of them are inserted, the default maximum value is 10 and the user time mode is set to false. 
 
-* a menu (@afonsocastro I will create this)
+There are 2 game modes: by number of typed letters and by time in seconds.
+The User Time Mode input argument sets the game mode. If its false (as default) the game mode is set by the number of the typed letters. In other hand, if the user call the utm input argument when launching the game, the game mode is set to the seconds of typing.
+The Maximum Value input argument sets the end of the test (in runaway seconds or in typed keys).
 
-* the instructions to install the program (@GuilhomenAguiar your instructions to install the required packages)
+For example, if the user run the command 
+    
+    main.py -mv 50 -utm
+    
+The game will end after 50 seconds of the moment that began.
 
-* the instructions to use the program (@zebastos similar to the help menu)
------------------------------------------------------------------------------------------------------
-To launch the program the user can enter 2 additional input arguments.If nothing inserted, default value is 10 and game mode is by attemps!
-
-The first argument can only be an integer and defines the number of insertions or the maximum playing time, if second argument, is 'utm'. This second arg, changes the game mode from attemp to time-based.
------------------------------------------------------------------------------------------------------
 After the program is lunched, user will be informed about the game configurations, and will be asked to press a key.
 After that action, a 3-second countdown will take place and the letters to type start to be requested.
 
 When the time limit or attempts are reached, the statistics of the game are presented.
 
+
+# Results
+
+After the end of the test, the game will show to user his performance, by displaying some indicators values.
+The list of the presented results is show in the following the table:
+
+Parameter | Description 
+--- | --- 
+test_duration | total test duration 
+test_start | begin test date 
+test_end | ends test date
+number_of_types | number of inputs 
+number_of_hits | number of correct inputs 
+accuracy | inputs accuracy (hits/total) 
+type_average_duration | average duration of the user answers 
+type_hit_average_duration | average duration of the correct user answers 
+type_miss_average_duration | average duration of the wrong user answers 
+types | list of information of each user answer: letter requested, letter received and ellapsed answer time (duration) 
+
+With all this suitable program, we hope that you could improve your typing skills.
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import getch
 import time
 import argparse
@@ -9,6 +10,10 @@ import random
 from collections import namedtuple
 import pprint
 import datetime
+
+from pyfiglet import Figlet
+
+
 Input = namedtuple("Input", ['requested', 'received', 'duration'])
 
 
@@ -17,26 +22,24 @@ number_typed_letters = 0
 hits = 0
 misses = 0
 total_time = 0
+
 # help context and input args
-parser = argparse.ArgumentParser(description=' This program aims to measure your typing accuracy!', epilog="And now you can pass to action!")
-parser.add_argument('max_value', metavar='MaxValue', nargs='?',
-                    help='As the first argument' + Fore.RED + ' MaxValue' + Fore.BLACK + ', please insert an integer number. By default is 10!\n',
-                    action="store", default=10, type=int)
-parser.add_argument('utm', metavar='UserTimeMode', nargs='?',
-                    help='As second argument, please insert:' + Fore.RED + ' utm' + Fore.BLACK + ' to play (MaxValue) seconds, if else, you play (MaxValue) atemps',
-                    action="store", type=str, default=False)
+parser = argparse.ArgumentParser(description=' This program aims to measure your typing accuracy.', epilog="And now you are ready to play!")
+
+
+parser.add_argument('-mv', '--max_value', help="Please insert an integer number. " + Fore.LIGHTBLUE_EX + ' MaxValue' + Fore.RESET + ' is the max of seconds (if' + Fore.LIGHTBLUE_EX + ' utm' + Fore.RESET + ' is set true) or the max of attempts (if' + Fore.LIGHTBLUE_EX + ' utm' + Fore.RESET + ' is set false). By default, ' + Fore.LIGHTBLUE_EX + ' MaxValue' + Fore.RESET + ' is 10.\n', action="store", default=10, type=int)
+
+parser.add_argument('-utm', '--utm', help='Please select:' + Fore.LIGHTBLUE_EX + ' utm' + Fore.RESET
+                                          + ' to play (' + Fore.LIGHTBLUE_EX + 'MaxValue' + Fore.RESET + ') seconds. Otherwise, you will play (' + Fore.LIGHTBLUE_EX + 'MaxValue' + Fore.RESET + ') attempts', action="store_true", default=False)
 
 
 tempo_do = parser.parse_args()
-# print (tempo_do.utm)
 
-if tempo_do.utm == 'utm':
-    game_mode = True  # play number by time
-else:
-    game_mode = False  # play by number of attempts
-# print (game_mode)
+# ARGUMENTS
+max_value = tempo_do.max_value
+utm = tempo_do.utm
 
-# print(tempo_do.__dict__["one"])
+
 TupList = []
 Timehitlist = []
 Timemisslist = []
@@ -45,7 +48,47 @@ dictionary = {"accuracy": "", "number of hits": "", "number of types": "", "test
               "type miss average time": "", "types": ""}
 
 
-def gameOn(letters):
+def delete_last_line():
+    """
+    Deletes the last line in the terminal
+    """
+
+    #cursor up one line
+    sys.stdout.write('\x1b[1A')
+
+    #delete last line
+    sys.stdout.write('\x1b[2K')
+
+
+def game_count_down(n):
+    """
+    Show the seconds (in a pretty way) to start the game
+    :param n: number of seconds that remains to start the test
+    """
+
+    f = Figlet(font='small')
+    print (Fore.LIGHTBLUE_EX)
+    print f.renderText(str(n))
+    print (Fore.RESET)
+    time.sleep(1)
+    delete_last_line()
+    delete_last_line()
+    delete_last_line()
+    delete_last_line()
+    delete_last_line()
+    delete_last_line()
+    delete_last_line()
+    delete_last_line()
+
+
+def game_on(letters):
+    """
+    Pick a random letter and ask the user to type that same letter.
+    Wait for the user answer and then save the correct or the wrong answers number. Besides this, it also saves the time that took the user to type the requested letter.
+    :param letters: string with all the possible letter for the program to ask
+    :return char: character that the user has pressed
+    """
+
     letter = random.choice(letters)
     print("Type letter " + Fore.LIGHTBLUE_EX + str(letter) + Fore.RESET)
     init_time = time.time()
@@ -61,7 +104,7 @@ def gameOn(letters):
         global hits
         hits += 1
         Timehitlist.append(elapsed_time)
-    else:
+    elif char != " ":
         print("You typed letter " + Fore.RED + str(char) + Fore.RESET)
         global misses
         misses += 1
@@ -71,72 +114,43 @@ def gameOn(letters):
     return char
 
 
-def printAllCharsUpTo(stop_char):
-    """
-    Pythonic version!
-    :param stop_char:
-    :return:
-    """
-    # to install getch
-    # pip install https://pypi.python.org/packages/source/g/getch/getch-1.0-python2.tar.gz#md5=586ea0f1f16aa094ff6a30736ba03c50
-    inputs = []
-
-    while True:
-        start_time = time.time()
-        char = getch.getch()  # also displayed on the screen
-        final_time = time.time()
-        print("Time elapsed is " + str(final_time - start_time) + "seconds")
-        print("You have entered  " + char)
-        if char == 'X':
-            break
-        else:
-            inputs.append(char)
-
-    print('Here is the list of all your inputs: ' + str(inputs))
-
-
 def main():
 
     print("Welcome to the " + Fore.RED + "PARI " + Fore.LIGHTBLUE_EX + "Ultimate Speed Typing Test." + Fore.RESET)
-    if not game_mode:
+    if not utm:
         print("You will play for "+Fore.RED + "{}".format(tempo_do.max_value) + Fore.RESET +
               " attempts".format(tempo_do.max_value))
     else:
-        print("You will play for "+Fore.RED + "{}".format(tempo_do.max_value) + Fore.RESET+" seconds")
+        print("You will play for " + Fore.RED + "{}".format(tempo_do.max_value) + Fore.RESET+" seconds")
+    print("You can quit the test, at any time, by pressing the spacebar key.")
     print("Please, when you're ready, press any key.")
     char = getch.getch()
 
-    print("The game will start in " + Fore.LIGHTBLUE_EX + "3" + Fore.RESET)
-    time.sleep(1)
-
-    print("The game will start in " + Fore.LIGHTBLUE_EX + "2" + Fore.RESET)
-    time.sleep(1)
-
-    print("The game will start in " + Fore.LIGHTBLUE_EX + "1" + Fore.RESET)
-    time.sleep(1)
-    print(Fore.LIGHTBLUE_EX+'          GOOD LUCK'+Fore.RESET)
+    print("The game will start in:")
+    game_count_down(3)
+    game_count_down(2)
+    game_count_down(1)
+    # print(Fore.LIGHTBLUE_EX+'          GOOD LUCK'+Fore.RESET)
 
     letters = string.ascii_letters[0:26]
     init_time = time.time()
     init_date = datetime.datetime.now()
 
-    # ARGUMENTS
-
-    max_value = tempo_do.max_value
-    utm = game_mode
-
     if not utm:
         while number_typed_letters < max_value:
-            char = gameOn(letters)
+            char = game_on(letters)
             global total_time
             total_time = time.time() - init_time
             if char == " ":
-                print("Uh oh,\nYou've cancelled the Ultimate Type Test.")
+                print("\n Uh oh! You've cancelled the " + Fore.LIGHTBLUE_EX + "Ultimate Speed Typing Test " + Fore.RESET + ".")
                 quit()
 
     else:
         while time.time()-init_time < max_value:
-            char = gameOn(letters)
+            char = game_on(letters)
+            if char == " ":
+                print("\nUh oh! You've cancelled the " + Fore.LIGHTBLUE_EX + "Ultimate Speed Typing Test " + Fore.RESET + ".")
+                quit()
         if time.time() - init_time <= max_value:
             total_time = float(time.time() - init_time)
             print("Current test duration is " + str(total_time))
@@ -154,27 +168,21 @@ def main():
     dictionary["accuracy"] = str('{:0.2f}'.format(float(hits)/(float(hits) + float(misses)) * 100)) + " %"
     dictionary["number of hits"] = hits
     dictionary["number of types"] = hits + misses
-    dictionary["test duration"] = '{:0.2f}'.format(total_time)
+    dictionary["test duration"] = '{:0.2f}'.format(total_time) + " seconds"
     dictionary["test start"] ='{:%Y-%m-%d %H:%M.%S}'.format((init_date))
     dictionary["test end"] = '{:%Y-%m-%d %H:%M.%S}'.format((final_date))
-    dictionary["type average time"] = '{:0.2f}'.format((TimeHit + Timemiss)/(hits + misses))
-    if hits == 0:
-        dictionary["type hit average time"] = 0.0
-    else:
-        dictionary["type hit average time"] = '{:0.2f}'.format(TimeHit / hits)
-    if misses == 0:
-        dictionary["type miss average time"] = 0.0
-    else:
-        dictionary["type miss average time"] = '{:0.2f}'.format(Timemiss / misses)
+    dictionary["type average time"] = '{:0.2f}'.format((TimeHit + Timemiss)/(hits + misses)) + " seconds"
+
+    dictionary["type hit average time"] = '{:0.2f}'.format(TimeHit / hits) + " seconds" if hits != 0 else str(0) + " seconds"
+
+    dictionary["type miss average time"] = '{:0.2f}'.format(Timemiss / misses) + " seconds" if misses != 0 else str(0) + " seconds"
+
     dictionary["types"] = map(str, TupList)
     pprint.pprint(dictionary)
-    print(Fore.RED +"          GAME OVER             " +Fore.RESET)
-    # print('\n'.join(map(str, TupList)))
-
-    # not displayed in the screen
-    # print('You have entered  ' + char)
-    # printAllCharsUpTo(char)
+    print("\n" + Fore.LIGHTBLUE_EX +"          GAME OVER             " +Fore.RESET)
 
 
 if __name__ == '__main__':
     main()
+
+
